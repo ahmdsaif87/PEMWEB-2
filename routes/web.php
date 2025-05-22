7406bb43d26ad0000971769f73c28485d92efbf8
ahmadsaif87 <ahmadsaif7456@gmail.com>
2025-05-22 17:19:00 +0700

    Make Regis and Login for User

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,7 @@ use App\Http\Controllers\homepageController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductCategoryController;
 use App\Http\Controllers\ProductsController;
-
-// use Livewire\Volt\Volt;
+use App\Http\Controllers\CustomerAuthController;
 
 Route::get('/', [homepageController::class, 'index'])->name('home');
 Route::get('products', [homepageController::class, 'products']);
@@ -20,16 +19,31 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
-Route::group(['prefix'=>'dashboard'], function () {
+Route::group(['prefix' => 'dashboard'], function () {
     Route::resource('categories', ProductCategoryController::class);
     Route::resource('products', ProductsController::class);
+});
+
+Route::group(['prefix' => 'customer'], function () {
+    Route::controller(CustomerAuthController::class)->group(function () {
+        //tampilkan halaman login
+        Route::get('login', 'login')->name('customer.login');
+        //aksi login
+        Route::post('login', 'store_login')->name('customer.store_login');
+        //tampilkan halaman register
+        Route::get('register', 'register')->name('customer.register');
+        //aksi register
+        Route::post('register', 'store_register')->name('customer.store_register');
+        //aksi logout
+        Route::post('logout', 'logout')->name('customer.logout');
+    });
 });
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
     Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
     Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance','settings.appearance')->name('settings.appearance');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
